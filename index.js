137bd62851798352ea2e539af4ce4dfcb587929a
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-29 00:52:55 +0600

    initial setp

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('Buy Sell server is running');
+})
+
+app.listen(port, () => console.log(`Buy Sell running on ${port}`))
