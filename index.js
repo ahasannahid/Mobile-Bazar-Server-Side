448cae6dca44de46f44485e9ca7be438a0c89597
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-29 12:39:15 +0600

    created api for save user

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// dbpassward = m0JkEgL3po6eHtDn
+// name = mobileBazar
 
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = "mongodb+srv://mobileBazar:m0JkEgL3po6eHtDn@cluster0.qbh9oi5.mongodb.net/?retryWrites=true&w=majority";
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const usersCollection = client.db('mobileBazar').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.log)
+
 
 app.get('/', async (req, res) => {
     res.send('Buy Sell server is running');
